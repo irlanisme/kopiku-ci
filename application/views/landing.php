a66ace76509506e771a5d62ec4b74044d699b7d7
irlan <irlanpranoto36@guru.smk.belajar.id>
2022-09-06 14:52:58 +0700

    beta1

---
+++ application/views/landing.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="<?php echo base_url()?>/resources/bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Hero -->
+<header >
+    <h1 class="text-center">Boost your day with <br> a cup of coffee</h1>
+    <div>
+        <img src="<?php echo base_url ("resources/img/hero2.jpg");?>">
+</div>
+</header>
+
+<!-- About -->
+<section>
+    <div>
+        <div class="row">
+            <div class="col-6">
+                <img height="600px" src="<?php echo base_url ("resources/img/hero2.jpg");?>"/>
+            </div>
+                <div class="col-6"></div>
+</div>
+</div>
+
+    <script src="<?php echo base_url()?>/resources/bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
